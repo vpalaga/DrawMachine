43079e1ea1134db586da700ba6e6c7fc45d06a87
vit palaga <vit@palaga.sk>
2025-12-27 20:36:21 +0200

    start with c

---
+++ MotorControll/PICO.c
@@ -0,0 +1,16 @@
+// Header file for input output functions
+// do the pico code here
+#include <stdio.h>
+
+// Main function: entry point for execution
+int main() {
+    int i;
+
+    // Writing print statement to print hello world
+    for (i = 0; i < 1000; i++){
+        printf("Hello World");
+
+    }
+
+    return 0;
+}
