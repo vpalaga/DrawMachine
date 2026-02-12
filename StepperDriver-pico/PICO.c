d6b16045c95381344fc678b9252bce8e2d6e2072
Vit Palaga <163043916+vpalaga@users.noreply.github.com>
2026-02-12 21:12:13 +0000

    sketch the c

--- StepperDriver-pico/PICO.c
+++ StepperDriver-pico/PICO.c
@@ -1,5 +1,9 @@
 // Header file for input output functions
 // do the pico code here
+/*
+will run a while (1) loop reciving and sending data thru the usb port (for now) to raspberry pi
+drive the TMCs 2209 drivers real time (same time data send)
+*/
 #include <stdio.h>
 
 // Main function: entry point for execution
