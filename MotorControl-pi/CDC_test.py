156fc1dbaee783d0e8d02a7e6d7093e533271195
Vejtah <vit@palaga.sk>
2026-02-16 21:46:41 +0100

    measure instruction send, recive confirmation time ~1ms - very happy

--- MotorControl-pi/CDC_test.py
+++ MotorControl-pi/CDC_test.py
@@ -1,17 +1,19 @@
 from CDC_send import Transmiter
 import time
-
-# Return a single random word
+import time
 
 transm = Transmiter()
+wait = 1
 
 try:
     while True:
-        recive_responce = transm.send_and_recive("WAT 3" + "\n")
-        print("recive " + str(recive_responce))
+        start_time = time.time()
+
+        receive_response = transm.send_and_recive("WAT " + str(wait) + "\n")
+        print("recive " + str(receive_response))
 
         finish_state = transm.send_and_recive(None)
-        print("finish " + str(finish_state))
+        print("finish " + str(finish_state) + " program time: " + str(round((time.time() - start_time - wait)*1000, ndigits=5)) + " miliseconds")
 
         time.sleep(1)
 finally:
